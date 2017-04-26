d43181119d1674ff899509f4416e77be79cdcc04
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-04-26 19:10:45 +0300

    [Dob commit] User model created. Validation need to be implemented.

---
+++ public/models/user.js
@@ -0,0 +1,33 @@
+class User {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    get username() {
+        return this._username;
+    }
+
+    set username(value) {
+        this._username = value;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(value) {
+        this._email = value;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    set password(value) {
+        this._password = value;
+    }
+}
+
+export { User };
