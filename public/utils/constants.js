04a4994f6d2e30895c409f6b6ab76bfd720a70c5
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-05-02 00:43:00 +0300

    MKey added.

--- public/utils/constants.js
+++ public/utils/constants.js
@@ -2,9 +2,9 @@ let constants = (() => {
     const kinveyInfo = {
         appKey: 'kid_SJVFb-Gyb',
         appSecret : 'f848e14f91454d548965f68c61bf2638',
-        hostUrl: 'https://baas.kinvey.com/'
+        hostUrl: 'https://baas.kinvey.com/',
+        masterKey: 'aea4dc78de9b4d3fac8a5189a6074d1a'
     };
-
 
     return {
         kinveyInfo: kinveyInfo
