ab315d912951a2dcd368a0c6a5a484f0d7f70fe8
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-05-01 16:42:29 +0300

    Start working on advertController.

---
+++ public/controllers/advertController.js
@@ -0,0 +1,55 @@
+import { Ad } from 'ad';
+import { data } from 'data';
+import { templateEngine } from 'templateEngine';
+import { homeController } from 'homeController';
+
+
+let adController = (() => {
+
+    function addNewAd() {
+        homeController.loadWelcomeMessage();
+
+        $('#wrapper').load('public/templates/addNewAd.html', null, () => {
+            $("#submitAdInfo-btn").on('click', (ev) => {
+                let title = $('#title').val(),
+                    make = $('#make').val(),
+                    model = $('#model').val(),
+                    price = $('#price').val(),
+                    fuel = $('#fuel').val(),
+                    power = $('#power').val(),
+                    mileage = $('#mileage').val(),
+                    gearbox = $('#gearbox').val(),
+                    manufactureDate = $('#manufactureDate').val();
+
+                let newAd = new Ad(title, make, model, price, fuel, power, mileage, gearbox, manufactureDate);
+
+                data.addNewAd(newAd)
+                    .then(() => {
+                        location.hash = '/myAds';
+                    });
+
+            });
+        });
+
+
+    }
+
+    function displayAds() {
+        homeController.loadWelcomeMessage();
+
+        data.getUserAds()
+            .then((userAds) => {
+                templateEngine.renderTemplate('myAds', userAds, '#wrapper');
+            } )
+    }
+
+
+    return {
+        addNewAd,
+        displayAds
+    }
+
+
+})();
+
+export { adController };
