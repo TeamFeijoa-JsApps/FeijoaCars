6a58b8f5a72cae36354e35797b531f22410a31ac
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-05-02 08:21:17 +0300

    Mkey removed.

--- public/utils/constants.js
+++ public/utils/constants.js
@@ -2,8 +2,7 @@ let constants = (() => {
     const kinveyInfo = {
         appKey: 'kid_SJVFb-Gyb',
         appSecret : 'f848e14f91454d548965f68c61bf2638',
-        hostUrl: 'https://baas.kinvey.com/',
-        masterKey: 'aea4dc78de9b4d3fac8a5189a6074d1a'
+        hostUrl: 'https://baas.kinvey.com/'
     };
 
     return {
